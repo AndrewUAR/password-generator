c5b096ae1c1122c61e4f2a1c73d89168a80b51b4
Andrew <andrewriznykua93@gmail.com>
2020-04-06 16:17:46 -0500

    Main component basic structure created

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Header from './header/Header';
+
+const Main = () => {
+  return (
+    <Fragment>
+      <Header />
+      <h3>Display component</h3>
+    </Fragment>
+  )
+}
+
+export default Main;
+
+
