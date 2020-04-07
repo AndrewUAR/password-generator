e778678dc00fd6192b4e2a2fdbde2d74f4cfdd2f
Andrew <andrewriznykua93@gmail.com>
2020-04-06 23:45:10 -0500

    useState hooks added, setPassword, setRange, setPasswordProps and passed to Container component

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,16 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './Display.css'
 import Container from '../container/Container';
 import Button from '../button/Button';
+import { generatePassword } from '../utils/Helper';
 
 const Display = () => {
+  const [password, setPassword] = useState('');
+  const [rangeValue, setRange] = useState();
+  const [passwordProps, setPasswordProps] = useState();
+
+  const generateNewPassword = () => {
+    const pwd = generatePassword(passwordProps, rangeValue);
+    setPassword(pwd);
+  }
+
   return (
     <Fragment>
       <div className="row">
         <div className="col-12 password-display-container">
-          <div>
+          <div style={{ width: '100%' }}>
             <div className="password-display">
-              <input type="text" className="password-display-input" value="dsfsdfdsf903249kj0" readOnly/>
+              <input type="text" className="password-display-input" value={password} readOnly/>
             </div>
             <div className="password-description">
               <i className="fas fa-check-circle"></i> Strong password
@@ -24,11 +34,16 @@ const Display = () => {
             <Button
               className="generate-btn"
               iconClass="fas fa-sync-alt"
+              handleClick={() => generateNewPassword()}
             />
           </div>
         </div>
       </div>
-      <Container />
+      <Container 
+        setPassword={setPassword} 
+        setRange={setRange} 
+        setPasswordProps={setPasswordProps}
+      />
     </Fragment>
   )
 }
