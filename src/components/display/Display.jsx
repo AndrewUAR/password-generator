1f8760cbf2540c2d4d69dcd89036059109399065
Andrew <andrewriznykua93@gmail.com>
2020-04-07 00:05:47 -0500

    setBackgroundColor function implemented

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -9,21 +9,41 @@ const Display = () => {
   const [rangeValue, setRange] = useState();
   const [passwordProps, setPasswordProps] = useState();
 
+  let passwordDescription = '';
+
   const generateNewPassword = () => {
     const pwd = generatePassword(passwordProps, rangeValue);
     setPassword(pwd);
   }
 
+  const setBackgroundColor = password => {
+    if (password && password.length >= 1 && password.length <= 5) {
+      passwordDescription = 'Bad password';
+      return '#cb473e';
+    } else if (password && password.length >= 6 && password.length <= 10) {
+      passwordDescription = 'Weak password';
+      return '#f07d58';
+    } else {
+      passwordDescription = 'Strong password';
+      return '#55a95d';
+    }
+  }
+
   return (
     <Fragment>
       <div className="row">
-        <div className="col-12 password-display-container">
+        <div className="col-12 password-display-container" style={{ backgroundColor: setBackgroundColor(password)}}>
           <div style={{ width: '100%' }}>
             <div className="password-display">
               <input type="text" className="password-display-input" value={password} readOnly/>
             </div>
             <div className="password-description">
-              <i className="fas fa-check-circle"></i> Strong password
+              {
+                password && password.length > 10 
+                ? <> <i className="fas fa-check-circle"></i> {passwordDescription} </>
+                : <> <i className="fas fa-exclamation-circle"></i> {passwordDescription} </>
+
+              }
             </div>
           </div>
           <div className="password-display-icons">
