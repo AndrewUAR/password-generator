ea94b9b640114e43426035951c493f8fcb45332c
Andrew <andrewriznykua93@gmail.com>
2020-04-06 22:11:39 -0500

    useState and useRef hook added

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import './Slider.css';
 
 const Slider = props => {
   const { step, min, max, value, defaultLength, onChangeValue } = props;
 
+  const rangeRef = useRef();
+  const [range, setRange] = useState();
+
+  const activeRangeColor = '#4aa1f3';
+  const rangeBackground = '#d7dcdf';
+
   const handleChange = max => e => {
     onChangeValue(e);
+    const value = e.target.value;
+    const progress = (value / max) * 100 + '%';
+    setRange(value);
+    const newBackgroundStyle = `linear-gradient(90deg, ${activeRangeColor} 0% ${progress}, ${rangeBackground} ${progress} 100%)`;
+    rangeRef.current.style.background = newBackgroundStyle;
   }
+
+  if (range !== defaultLength || !range) {
+    setRange(defaultLength);
+  }
+
+  const progressValue = range;
+  const progress = (progressValue / max) * 100 + '%';
+  const styleInput = {
+    background: `linear-gradient(90deg, ${activeRangeColor} 0% ${progress}, ${rangeBackground} ${progress} 100%)`
+  }
+
   return (
     <div className="slider-container">
       <div className="slider">
-        <input 
+        <input
+          ref={rangeRef} 
           className="range-slider"
           type="range"
           step={step}
@@ -20,8 +43,9 @@ const Slider = props => {
           defaultLength={defaultLength}
           value={value}
           onChange={handleChange(max)}
+          style={styleInput}
         />
-        <span className="range-slider-value">10</span>
+        <span className="range-slider-value">{progressValue}</span>
       </div>
     </div>
   )
